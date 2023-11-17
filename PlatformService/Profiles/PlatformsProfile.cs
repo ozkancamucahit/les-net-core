21b47576096dfaf811f597d81dd1a0f62bdd6218
MMucahit <ozkancamucahit@gmail.com>
2023-11-17 10:00:04 +0300

    mapper for platform

---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using PlatformService.DTOS;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public sealed class PlatformsProfile : Profile
+    {
+        public PlatformsProfile()
+        {
+            // Source => Target 
+
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+
+        }
+
+        
+
+
+    }
+}
