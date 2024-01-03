d703572dea63a717dff391c276883fba8bb0606c
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-03 16:46:34 +0300

    communication class for platforms added

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+	[Route("api/c/[controller]")]
+	[ApiController]
+	public sealed class PlatformsController : ControllerBase
+	{
+
+		#region CTOR
+		public PlatformsController()
+		{
+			
+		}
+		#endregion
+
+
+		[HttpPost]
+		public ActionResult TestConnection()
+		{
+			Console.WriteLine(" ==> POST INCOMING COMMAND SERVICE");
+			return Ok("COMPLETED");
+		}
+	}
+}
