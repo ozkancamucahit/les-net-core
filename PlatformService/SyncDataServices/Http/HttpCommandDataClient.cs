e09ba9fe3188e2bc81fc689ab9d239911887ac0b
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-04 13:18:12 +0300

    Http client implementation added

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,44 @@
+﻿using PlatformService.DTOS;
+using System.Text;
+using System.Text.Json;
+
+namespace PlatformService.SyncDataServices.Http
+{
+	public sealed class HttpCommandDataClient : ICommandDataClient
+	{
+		#region FIELDS
+		private readonly HttpClient _httpClient;
+		private readonly IConfiguration configuration;
+
+		#endregion
+
+		#region CTOR
+		public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+			this.configuration = configuration;
+		}
+        #endregion
+        public async Task SendPlatformToCommand(PlatformReadDto platform)
+		{
+			var httpContent = new StringContent(
+					JsonSerializer.Serialize(platform),
+					encoding: Encoding.UTF8,
+					mediaType: "application/json"
+				);
+
+			var response = await _httpClient.PostAsync(configuration["CommandService"], httpContent);
+
+			if(response.IsSuccessStatusCode)
+			{
+				Console.WriteLine(" ==> POST : OK");
+			}
+			else
+			{
+
+				Console.WriteLine(" ==> POST : NOT OK");
+			}
+
+		}
+	}
+}
---
+++ PlatformService/SyncDataServices/Http/ICommandDataClient.cs
@@ -0,0 +1,9 @@
+﻿using PlatformService.DTOS;
+
+namespace PlatformService.SyncDataServices.Http
+{
+	public interface ICommandDataClient
+	{
+		Task SendPlatformToCommand(PlatformReadDto platform);
+	}
+}
