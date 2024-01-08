c371624940e6c50b83d5415c5ad20d99e96ea852
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-08 14:17:15 +0300

    CS platforms controller endpoint added for plats

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,19 +1,36 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.DTOS;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
 {
-	[Route("api/c/[controller]")]
+	[Route("api/c/[controller]/[action]")]
 	[ApiController]
 	public sealed class PlatformsController : ControllerBase
 	{
+		private readonly ICommandRepo repo;
+		private readonly IMapper mapper;
 
 		#region CTOR
-		public PlatformsController()
+		public PlatformsController(ICommandRepo repo, IMapper mapper)
 		{
-			
+			this.repo = repo;
+			this.mapper = mapper;
 		}
 		#endregion
+
+
+		[HttpGet]
+		public ActionResult<IEnumerable<PlatformReadDTO>> AllPlatforms()
+		{
+			Console.WriteLine(" ==> GETTING PLATFORMS FROM COMMANDS SERVICE");
+
+			var platforms = repo.GetAllPlatforms();
+
+			return Ok(mapper.Map<IEnumerable<PlatformReadDTO>>(platforms));
+		}
 
 
 		[HttpPost]
