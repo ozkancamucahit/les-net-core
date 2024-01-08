738d4a6261307094cac428284b267ad43da1b46e
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-08 14:16:25 +0300

    mapper profile for commandsSRV added

---
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CommandsService.DTOS;
+using CommandsService.Models;
+
+namespace CommandsService.Profiles
+{
+	public sealed class CommandsProfile : Profile
+	{
+        public CommandsProfile()
+        {
+			// Source => Target 
+
+			CreateMap<Platform, PlatformReadDTO>();
+			CreateMap<Command, CommandReadDTO>();
+			CreateMap<CommandCreateDTO, Command>();
+		}
+    }
+}
