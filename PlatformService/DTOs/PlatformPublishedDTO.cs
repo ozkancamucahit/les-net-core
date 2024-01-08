ddd5a883d44626d85d807d84b816440df4867efc
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-08 17:19:56 +0300

    Platform publish model added

---
+++ PlatformService/DTOs/PlatformPublishedDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace PlatformService.DTOS
+{
+	public sealed class PlatformPublishedDTO
+	{
+        public int Id { get; set; }
+
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Event { get; set; } = string.Empty;
+
+
+    }
+}
