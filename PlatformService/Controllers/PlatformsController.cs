431e338d7f13a5a2acc09c303dd75ca0e7ed7679
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-08 17:20:40 +0300

    messagebus added to plat. controller

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,6 +1,7 @@
 
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.DTOS;
 using PlatformService.Models;
@@ -15,17 +16,20 @@ namespace PlatformService.Controllers
         private readonly IPlatformRepo repo;
         private readonly IMapper mapper;
 		private readonly ICommandDataClient commandDataClient;
+		private readonly IMessageBusClient messageBusClient;
 
-        #region CTOR
-        public PlatformsController(
+		#region CTOR
+		public PlatformsController(
                     IPlatformRepo repo,
                     IMapper mapper,
-                    ICommandDataClient commandDataClient)
+                    ICommandDataClient commandDataClient,
+                    IMessageBusClient messageBusClient)
         {
             this.repo = repo;
             this.mapper = mapper;
             this.commandDataClient = commandDataClient;
-        }
+			this.messageBusClient = messageBusClient;
+		}
         #endregion
 
         [HttpGet]
@@ -60,16 +64,31 @@ namespace PlatformService.Controllers
 
             var PlatformReadDto = mapper.Map<PlatformReadDto>(platformModel);
 
+            // SEND SYNC MESSAGE
+
+
             try
             {
                 await commandDataClient.SendPlatformToCommand(PlatformReadDto);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(" ==> COULD NOT CREATE" + ex.Message);
+                Console.WriteLine(" ==> COULD NOT SEND SYNCHRONOUSLY" + ex.Message);
             }
 
-            return CreatedAtRoute(nameof(GetPlatformById), new { Id = PlatformReadDto.Id}, PlatformReadDto);
+            // SEND AYSNC MESSAGE
+            try
+            {
+                var platformPublishedDTO = mapper.Map<PlatformPublishedDTO>(PlatformReadDto);
+                platformPublishedDTO.Event = "Platform_Published";
+                messageBusClient.PublishNewPlatform(platformPublishedDTO);
+            }
+            catch (Exception ex)
+			{
+				Console.WriteLine(" ==> COULD NOT SEND ASYNCHRONOUSLY" + ex.Message);
+			}
+
+			return CreatedAtRoute(nameof(GetPlatformById), new { Id = PlatformReadDto.Id}, PlatformReadDto);
 
         }
 
