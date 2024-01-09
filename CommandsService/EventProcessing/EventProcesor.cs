2bb4281bf8aed78123b28fb76d300ee15ad2b015
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-09 14:08:29 +0300

    addplatform added for commands

--- CommandsService/EventProcessing/EventProcesor.cs
+++ CommandsService/EventProcessing/EventProcesor.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using CommandsService.Data;
 using CommandsService.DTOS;
+using CommandsService.Models;
 using System.Text.Json;
 
 namespace CommandsService.EventProcessing
@@ -39,6 +41,39 @@ namespace CommandsService.EventProcessing
 					break;
 			}
 
+		}
+
+		private void AddPlatform(string  platformPublishedMessage)
+		{
+			using (var scope = scopeFactory.CreateScope())
+			{
+				var repo = scope.ServiceProvider.GetRequiredService<ICommandRepo>();
+
+				var platformPublishedDTO = JsonSerializer.Deserialize<PlatformPublishedDTO>(platformPublishedMessage);
+
+				try
+				{
+					var plat = mapper.Map<Platform>(platformPublishedDTO);
+
+					if(!repo.ExternalPlatformExists(plat.ExternalID))
+					{
+						repo.CreatePlatform(plat);
+						repo.SaveChanges();
+                        Console.WriteLine(" ==> ADDED PLATFORM");
+                    }
+					else
+					{
+                        Console.WriteLine(" ==> PLATFORM ALREADY EXISTS");
+                    }
+
+				}
+				catch (Exception ex)
+				{
+                    Console.WriteLine(" ==> COULD NOT ADD PLATFORM TO DB :" + ex.Message);
+                    throw;
+				}
+
+			}
 		}
 
 		private EventType DetermineEvent(string NotificationMessage)
