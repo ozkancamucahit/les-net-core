f009f8883cd0a7a955305b48ffc495cf32591870
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-01-21 14:11:14 +0300

    grpc added in program for platforms

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,7 +37,10 @@ else if (builder.Environment.IsProduction())
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
-builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+
+builder.Services
+    .AddSingleton<IMessageBusClient, MessageBusClient>()
+    .AddGrpc();
 
 var app = builder.Build();
 
@@ -54,6 +58,12 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcPlatformService>();
+app.MapGet("/protos/Platforms.proto", async (context) => {
+    await context.Response.WriteAsync(File.ReadAllText("Protos/Platforms.proto"));
+});
+
+
 PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
 app.Run();
